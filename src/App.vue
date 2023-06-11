2b11210ed924ab082c5c01a9036ab7317508ec97
Rahul Alam <gaberialcramern31@outlook.com>
2023-06-11 21:20:10 +0530

    update before routeer

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup>
+
+import { ref, watch } from "vue";
+import q from "./data/quizes.json";
+import Card from "./components/Card.vue"
+
+const quizes = ref(q);
+const search = ref("");
+
+watch(search, () => {
+  quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+})
+
+</script>
+
+
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizz Hunt</h1>
+      <input type="text" placeholder="Search..." v-model.trim="search">
+    </header>
+    <div class="options-container">
+      <Card v-for="quiz in quizes" :key="quiz.id" :data="quiz"/>
+      <!-- <div class="card" v-for="quiz in quizes" :key="quiz.id">
+        <img :src="quiz.img" alt="">
+        <div class="card-text">
+          <h2>{{quiz.name}}</h2>
+          <p>{{quiz.questions.length}} Questions</p>
+        </div>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  max-width: 1000px;
+  margin: 0 auto;
+}
+header{
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+header h1{
+  font-weight: bold;
+  margin-right: 30px;
+}
+header input{
+  border:none;
+  background-color: rgba(128, 128, 128,0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.options-container{
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40PX;
+}
+
+</style>
